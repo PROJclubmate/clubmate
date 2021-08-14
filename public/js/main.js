dc964414e22920d7a38a0c108ecb61e933195cd3
Ashutosh Guleria <ashutoshguleria9@gmail.com>
2021-08-15 00:57:21 +0530

    notion inspired landing page

--- public/js/main.js
+++ public/js/main.js
@@ -652,18 +652,18 @@ if(location.pathname.split('/').length == 3 && location.pathname.split('/')[1] =
   var coverTall = false;
   $('#college_scroll').scroll(function(){
     if(!coverTall){
-      if($('#college_scroll').scrollTop() > 116){
+      if($('#college_scroll').scrollTop() > 160){
         coverTall = true;
         if(window.innerWidth > 992){
           $('.college-cover-div').height(350);
           $('img#college-cover').removeClass('desktopFit');
         } else if(window.innerWidth <= 992 && window.innerWidth > 768){
-          $('.college-cover-div').height(200);
+          $('.college-cover-div').height(300);
           $('img#college-cover').removeClass('mobileblurred');
           $('.college-infodiv').addClass('nodisplay').removeClass('tabletShow');
           $('img#college-cover').removeClass('desktopFit');
         } else if(window.innerWidth <= 768){
-          $('.college-cover-div').height(200);
+          $('.college-cover-div').height(300);
           $('img#college-cover').removeClass('mobileblurred');
           $('.college-infodiv').addClass('nodisplay');
         }
