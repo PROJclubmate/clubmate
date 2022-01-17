62c1b9f585b9af3290846868956449b3a0cff3ef
= <awesomepranshu@gmail.com>
2022-01-18 04:37:59 +0530

    Add apis for audio rooms

--- routes/audio_rooms.js
+++ routes/audio_rooms.js
@@ -1,10 +1,11 @@
 const express   = require('express'),
   router        = express.Router(),
   middleware    = require('../middleware'),
-  {audioroomsLobby} = require('../controllers/audio_rooms');
+  {audioroomsLobby , newAudioroom} = require('../controllers/audio_rooms');
 
 
 // Get create story page
 router.get('/lobby', middleware.isLoggedIn, audioroomsLobby);
+router.post('/newAudioroom' , middleware.isLoggedIn , newAudioroom);
 
 module.exports = router;
