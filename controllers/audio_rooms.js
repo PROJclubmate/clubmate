ce3b2ba6dc07813fa85c63b83a2ad3459132c58d
= <awesomepranshu@gmail.com>
2022-01-18 04:37:59 +0530

    Add apis for audio rooms

--- controllers/audio_rooms.js
+++ controllers/audio_rooms.js
@@ -1,5 +1,59 @@
+const Club = require('../models/club'),
+  logger = require('../logger'),
+  User = require('../models/user'),
+  Audioroom = require('../models/audioroom');
+
 module.exports = {
-  audioroomsLobby(req, res, next){
-    return res.render('audio_rooms/lobby');
-  }
+
+  async newAudioroom(req, res, next) {
+    Club.findById(req.params.club_id, function (err, foundClub) {
+      if (err || !foundClub) {
+        logger.error(req.user._id + ' : (stories-2)foundClub err => ' + err);
+        req.flash('error', 'Something went wrong :(');
+        return res.redirect('back');
+      }
+      else {
+        for (let i = foundClub.clubUsers.length - 1; i >= 0; i--) {
+          if (foundClub.clubUsers[i].id.equals(req.user._id) && foundClub.clubUsers[i].userRank <= 2) {
+            audioroom = new Audioroom({
+              roomName: req.body.roomName,
+              roomId: req.body.roomId,
+              timestamp: Date.now(),
+              audioroomClub: foundClub._id,
+              audioroomCreator: req.user._id,
+              capacity: req.body.capacity,
+            });
+            audioroom.save();
+            foundClub.audiorooms.addToSet(audioroom);
+            foundClub.save();
+          }
+        }
+        return res.redirect('/clubs/' + req.params.club_id);
+      }
+    });
+  },
+
+  async audioroomsLobby(req, res, next){
+    // req.user._id
+    User.findById(req.user._id, async function (err, foundUser) {
+      if (err || !foundUser) {
+        req.flash('error', 'Something went wrong :(');
+        return res.redirect('back');
+      }
+      else{
+        audioroomsData = {}
+        for(var i = 0; i < foundUser.userClubs.length; i++){
+          let foundClub = await Club.findById(foundUser.userClubs[i]).exec();
+          if(foundClub && foundClub.audiorooms.length){
+            audioroomsData[foundClub.name] = []
+            for(var j = 0; j < foundClub.audiorooms.length; j++){
+              let foundAudioroom = await Audioroom.findById(foundClub.audiorooms[j]._id).exec();
+              audioroomsData[foundClub.name].push(foundAudioroom);
+            }
+          }
+        }
+        return res.render('audio_rooms/lobby' , {audioroomsData});
+      }
+    });
+  },
 };
---
+++ models/audioroom.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose'),
+  Schema       = mongoose.Schema;
+
+const audioroomSchema = new Schema({  
+  roomName: String,
+  roomId: String,
+  timestamp: Number,
+  usersInside: [{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  speakers: [{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  moderators: [{
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }],
+  audioroomClub: {
+    type: Schema.Types.ObjectId,
+    ref: 'Club'
+  },
+  audioroomCreator: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  capacity: Number,
+  isClubExclusive: {type: Boolean, default: true},
+  createdAt: {type: Date, default: Date.now},
+});
+
+audioroomSchema.index({audioroomClub: 1});
+audioroomSchema.index({createdAt: 1});
+
+module.exports = mongoose.model('Audioroom', audioroomSchema);
