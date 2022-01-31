f947b3e060fbe12485df3cb82b1029eb3eb068c5
Signior-X <b19188@students.iitmandi.ac.in>
2022-01-31 22:05:35 +0530

    Make club name to link in lobby

--- controllers/audio_rooms.js
+++ controllers/audio_rooms.js
@@ -80,8 +80,7 @@ module.exports = {
       for(var i = 0; i < foundUser.userClubs.length; i++){
         let foundClub = await Club.findById(foundUser.userClubs[i].id).exec();
         if(foundClub && foundClub.audiorooms.length){
-          let clubData = {};
-          clubData["club_name"] = foundClub.name;
+          let clubData = foundClub;
           clubData["audio_rooms"] = []
           for(var j = 0; j < foundClub.audiorooms.length; j++){
             let foundAudioroom = await Audioroom.findById(foundClub.audiorooms[j]._id).exec();
@@ -93,6 +92,7 @@ module.exports = {
           audioroomsData.push(clubData);
         }
       }
+
       return res.render('audio_rooms/lobby', { audioroomsData });
     }
     else{
