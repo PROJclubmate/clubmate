38e8e80b823db857e63645d3b6ba046e207b1d15
tinytrebuchet <tinytrebuchet@protonmail.com>
2022-04-03 22:53:06 +0530

    blog backend v5

---
+++ routes/blogs.js
@@ -0,0 +1,40 @@
+const middleware = require('../middleware');
+const express = require('express'),
+  router = express.Router();
+const { 
+  blogsLoadPage,
+  blogsCreatePage,
+  blogsCreate,
+  blogsDelete,
+  blogsSave,
+  blogsUnsave,
+  blogsSavedPage,
+  blogsHeart,
+  blogsUserPage,
+  blogsPublishPage,
+  blogsApprove,
+  blogsRemove,
+} = require('../controllers/blog');
+
+if (process.env.ENVIRONMENT === "dev") {
+  var { upload } = require("../config/cloudinary.js");
+} else if (process.env.ENVIRONMENT === "prod") {
+  var { upload } = require("../config/s3.js");
+}
+
+router.get('/colleges/:collegeName/blogs', middleware.isLoggedIn, blogsLoadPage);
+router.get('/colleges/:collegeName/blogs/new', middleware.isLoggedIn, blogsCreatePage);
+router.post('/colleges/:collegeName/blogs/blogs/new', middleware.isLoggedIn, blogsCreate);
+router.get('/colleges/:collegeName/blogs/blogs/publish', middleware.isLoggedIn, blogsPublishPage);
+router.put('/colleges/:collegeName/blogs/publish/approve', middleware.isLoggedIn, blogsApprove);
+router.put('/colleges/:collegeName/blogs/publish/remove', middleware.isLoggedIn, blogsRemove);
+router.get('/colleges/:collegeName/blogs/saved', middleware.isLoggedIn, blogsSavedPage);
+router.get('/colleges/:collegeName/blogs/user/:userId', middleware.isLoggedIn, blogsUserPage);
+router.put('/colleges/:collegeName/blogs/:bucket/:blog/save', middleware.isLoggedIn, blogsSave);
+router.put('/colleges/:collegeName/blogs/:bucket/:blog/unsave', middleware.isLoggedIn, blogsUnsave);
+router.put('/colleges/:collegeName/blogs/:bucket/:blog/heart', middleware.isLoggedIn, blogsHeart);
+router.delete('/colleges/:collegeName/blogs/:bucket/:blog', middleware.isLoggedIn, blogsDelete);
+
+
+
+module.exports = router;
